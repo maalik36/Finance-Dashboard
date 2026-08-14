5b73d31c6b180d25dfa9ec01291dc8f07b44b3ae
Maalik Brown <mbrown29@zagmail.gonzaga.edu>
2026-08-14 11:59:42 -0700

    fixed more imports

--- Backend/src/routes/transaction.ts
+++ Backend/src/routes/transaction.ts
@@ -2,15 +2,17 @@ import { Router } from "express";
 import { auth } from "../middleware/auth";
 import {
   getTransactions,
-  addTransaction
-} from "../controllers/transactionsController";
-import { getMonthlySummaryController } from "../controllers/transactionsController";
+  addTransaction,
+  //updateTransaction,
+  deleteTransaction
+} from "../controllers/transactionController";
+import { getMonthlySummaryController } from "../controllers/transactionController";
 const router = Router();
 
 router.get("/", auth, getTransactions);
 router.post("/", auth, addTransaction);
 router.get("/monthly-summary", auth, getMonthlySummaryController);
-router.put("/:id", auth, updateTransaction);
+//router.put("/:id", auth, updateTransaction);
 router.delete("/:id", auth, deleteTransaction);
 
 export default router;
